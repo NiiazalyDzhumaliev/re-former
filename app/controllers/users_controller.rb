63973099c7554c7da92bbb96cb9cc2e80c412c2f
hillarioh <okerioh@gmail.com>
2020-06-04 13:03:50 +0300

    add edit route, edit view, edit update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,20 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+
+        redirect_to new_user_path
+    end
+
+    
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
