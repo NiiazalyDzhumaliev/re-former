ef083868e84867676eadcf82c87efe5180d6a24d
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-04 14:56:29 +0600

    Comment the HTML form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         end
     end
 
-  private
+    private
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
