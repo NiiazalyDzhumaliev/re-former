ce006c9ec3932b013739c3d3ffa59b59b2ff22cc
hillarioh <okerioh@gmail.com>
2020-06-04 13:27:36 +0300

    edit update action, display error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,13 @@ class UsersController < ApplicationController
 
     def update
         @user = User.find(params[:id])
-        @user.update(user_params)
 
-        redirect_to new_user_path
+        if @user.update(user_params)
+
+            redirect_to new_user_path
+        else
+            render :edit
+        end
     end
 
     
